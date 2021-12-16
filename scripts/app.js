2e9cbfade60d90a877e36989ca1fb0f5003bbc47
wengjae <77240633+Jaee-C@users.noreply.github.com>
2021-12-16 21:18:27 +0800

    handle form submission - used FormData blueprint

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,19 @@
 
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backdrop = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
 
 const editPlayer1Btn = document.getElementById('edit-player-1');
 const editPlayer2Btn = document.getElementById('edit-player-2');
 const overlayCancelBtn = document.getElementById('overlay-cancel-btn');
 
+// Open config pop up window
 editPlayer1Btn.addEventListener('click', openPlayerConfig);
 editPlayer2Btn.addEventListener('click', openPlayerConfig);
 
+// Close config pop up window
 overlayCancelBtn.addEventListener('click', closePlayerConfig);
 backdrop.addEventListener('click', closePlayerConfig);
+
+// Player config form submission
+formElement.addEventListener('submit', savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -9,3 +9,11 @@ function closePlayerConfig() {
     playerConfigOverlay.style.display = 'none';
     backdrop.style.display = 'none';
 }
+
+// Handling player config form submission
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayerName = formData.get('playername');
+    console.log(enteredPlayerName);
+}
