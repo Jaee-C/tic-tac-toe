b0bc9369f939128854f577e38c6b09884ce6913b
wengjae <77240633+Jaee-C@users.noreply.github.com>
2021-12-16 21:03:26 +0800

    enable open and close pop up config window

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
 /** Variable declarations */
+
+const playerConfigOverlay = document.getElementById('config-overlay');
+const backdrop = document.getElementById('backdrop');
+
+const editPlayer1Btn = document.getElementById('edit-player-1');
+const editPlayer2Btn = document.getElementById('edit-player-2');
+const overlayCancelBtn = document.getElementById('overlay-cancel-btn');
+
+editPlayer1Btn.addEventListener('click', openPlayerConfig);
+editPlayer2Btn.addEventListener('click', openPlayerConfig);
+
+overlayCancelBtn.addEventListener('click', closePlayerConfig);
+backdrop.addEventListener('click', closePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,11 @@
 /** Logic for player configurations */
+
+function openPlayerConfig() {
+    playerConfigOverlay.style.display = 'block';
+    backdrop.style.display = 'block';
+}
+
+function closePlayerConfig() {
+    playerConfigOverlay.style.display = 'none';
+    backdrop.style.display = 'none';
+}
