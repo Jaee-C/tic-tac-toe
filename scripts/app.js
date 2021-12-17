96c21afa206674f408b242b0004d51b890c69fb9
wengjae <77240633+Jaee-C@users.noreply.github.com>
2021-12-17 10:56:40 +0800

    player config form validation and error output

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backdrop = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
+const errorsOutput = document.getElementById('config-error');
 
 const editPlayer1Btn = document.getElementById('edit-player-1');
 const editPlayer2Btn = document.getElementById('edit-player-2');
--- scripts/config.js
+++ scripts/config.js
@@ -8,12 +8,22 @@ function openPlayerConfig() {
 function closePlayerConfig() {
     playerConfigOverlay.style.display = 'none';
     backdrop.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutput.textContent = '';
 }
 
 // Handling player config form submission
 function savePlayerConfig(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const enteredPlayerName = formData.get('playername');
+    const enteredPlayerName = formData.get('playername').trim();
     console.log(enteredPlayerName);
+
+    // Form validation
+    if (!enteredPlayerName) {
+        event.target.firstElementChild.classList.add('error');
+        errorsOutput.textContent = 'Please enter a valid name';
+        return;
+    }
+
 }
